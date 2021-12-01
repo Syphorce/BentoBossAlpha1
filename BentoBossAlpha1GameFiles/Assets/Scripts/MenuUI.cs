ae1d15fc5101613fb7584b472eb955d1babe75e7
Syphorce <syphorce@gmail.com>
2021-12-01 15:49:18 -0500

    Menu UI and Game UI
    Created a quick and dirty Menu UI to start off the game.
    
    Game UI has simple directions on how to play.

---
+++ BentoBossAlpha1GameFiles/Assets/Scripts/MenuUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+ public void OnPlayButton()
+    {//Change "SampleScene" to "Game" when we eventually change the Sample Scene name to Game Scene
+        SceneManager.LoadScene("SampleScene");
+
+    }
+    
+ public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+}
